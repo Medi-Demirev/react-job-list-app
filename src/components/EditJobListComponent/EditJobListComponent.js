1882eb88fb63e79328307daaf86193e5cac88966
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-27 01:21:17 +0200

    Component refactoring and synchronization to work with the context

--- src/components/EditJobListComponent/EditJobListComponent.js
+++ src/components/EditJobListComponent/EditJobListComponent.js
@@ -1,32 +1,63 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import {JobListContext} from '../../context/JobListContext';
 import './EditJobListComponent.css';
 
-const EditJobListComponent = () => {
+const EditJobListComponent = ({currentData}) => {
     const [isOpen, setIsOpen] = useState(true);
+    const {data, editJob} = useContext(JobListContext);
+    const[inputs, setInputs] = useState({ priority:''});
+
+    const id = currentData.map(x=> x.id).pop();
+    const job = currentData.map(x =>x.job).pop();
+    const priority = inputs.priority;
+
+    const changeHandler = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        })
+    };
+    const updatedJob = {id, job, priority };
+    
 
     const closeHandler = () => {
         setIsOpen(false)
+        window.location.reload();
     };
-    console.log(isOpen);
+
+    const onSubmit = (e)=> {
+        e.preventDefault();
+        editJob(id, updatedJob)       
+    };
+  
+ 
     return isOpen ? (
         <div className="priority_edit">
+            <form onSubmit={onSubmit}>
+            
             <div className='close_btn_container'>
                 <button className='close_btn' type='submit' onClick={closeHandler}>x</button>
             </div>
-            <div className='border'>
-                <h4 className="priority_title">My jobs name 2</h4>
-                <select name="priority_select" className='priority_select'>
-
+                {currentData.map((currentJob)=>
+            <div className='border' key={currentJob.id}>
+                <h4 className="priority_title">{currentJob.job}</h4>
+                <select name="priority" className='priority_select'
+                onChange={changeHandler}
+                defaultValue={currentJob.priority}>
+                
                     <option value={'Urgent'} >Urgent</option>
-                    <option value={'Regular'}>Regular</option>
+                    <option value={"Regular"}>Regular</option>
                     <option value={'Trivial'}>Trivial</option>
                 </select>
-                <div className='edit_btn_container'>
-                    <button className='update_btn' >Update</button>
+                <div className='edit_btn_container' key={currentJob.id}>
+                    <button className='update_btn' type='submit' title='Update item'>Update</button>
                 </div>
             </div>
+            )}
+      
+        </form>
         </div>
-    ) : ''
+    ) : null
 };
 
 export default EditJobListComponent;
