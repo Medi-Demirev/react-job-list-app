d3a39fda197e4c4583c296b61b804dc454d908bb
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-27 01:22:54 +0200

    Add context provider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import logo from './logo.svg';
 import './App.css';
 import JobListComponent from './components/jobListComponent/JobListComponent';
+
+import JobListContextProvider from './context/JobListContext';
 import EditJobListComponent from './components/EditJobListComponent/EditJobListComponent';
 
-
 function App() {
+  
   return (
-    <div className="App">
-      <EditJobListComponent/>
+    <div className="container">
+      <JobListContextProvider>
       <JobListComponent/>
-      
+      </JobListContextProvider>
     </div>
   );
 }
