301655f1515e85852e7818836587dd67401b02f2
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-29 13:11:31 +0200

    Add filter data logic and fix edit bug

--- src/context/JobListContext.js
+++ src/context/JobListContext.js
@@ -7,8 +7,7 @@ const JobListContextProvider = (props) =>{
     const localStorageData = JSON.parse(localStorage.getItem("job_list")) || [];
     const [data, setData] = useState(localStorageData);
     const id = uuidv4();
-
-   
+    
     useEffect(() => {
         localStorage.setItem('job_list', JSON.stringify(data));
     },[data]);
@@ -22,18 +21,26 @@ const JobListContextProvider = (props) =>{
     };
 
     const editJob = (id, updatedJob) => {
-        setData(data.map((job) => job.id === id ? updatedJob : data))
+        setData(data.map((job) => job.id === id ? updatedJob : job))
     };
-    
+
     const order = { Urgent: 1, Regular: 2, Trivial: 3 };
     const sortedJobs = data.sort((a,b)=> order[a.priority] - order[b.priority]);
-    
+
+    const getFilteredData = (searchData, data) => {
+       if (!searchData) {
+        return data
+       }
+       return data.filter((job)=>job.job.toLowerCase().includes(searchData))
+    };
+
     return(
         <JobListContext.Provider  value={{
             addJob, 
             deleteJob,
             editJob,
             sortedJobs,
+            getFilteredData,
             data}}>
             {props.children}
         </JobListContext.Provider>
