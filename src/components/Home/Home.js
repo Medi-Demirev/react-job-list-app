8da1ba0a6b2176b466b21ab90a177013c87bbac2
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-29 13:06:37 +0200

    Create Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import AddJobComponent from '../AddJobComponent/AddJobComponent';
+import './Home.css'
+
+const Home = () =>{
+
+    return(
+        <div className="container_wrap">
+            <AddJobComponent/>
+        </div>
+    )
+};
+
+export default Home;
