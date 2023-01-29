2cff5e598ba558b95b46269c3dfbb1c29b2beeb7
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-29 15:58:10 +0200

    Creating validation constants

---
+++ src/constants/Validations/validations.js
@@ -0,0 +1,9 @@
+
+export const JOB_NAME_VALIDATION = {
+    JOB_NAME_REGEX:/^[a-z]+[a-z0-9]*$/i,
+    JOB_NAME_ERROR_MSG: "Please use only English letters",
+
+    JOB_NAME_MAX_LENGTH: 70,
+    JOB_NAME_MIN_LENGTH: 1, 
+    JOB_NAME__ERROR_MSG: "Name must be between 1 and 70 symbols.",
+};
