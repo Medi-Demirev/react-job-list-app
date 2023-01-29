63bf32b686f11ee060bb6b96a09016e09a47bbf4
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-29 16:10:59 +0200

    Fix regex bug

--- src/constants/Validations/validations.js
+++ src/constants/Validations/validations.js
@@ -1,6 +1,6 @@
 
 export const JOB_NAME_VALIDATION = {
-    JOB_NAME_REGEX:/^[a-z]+[a-z0-9]*$/i,
+    JOB_NAME_REGEX:/^[a-zA-Z0-9\s]*$/,
     JOB_NAME_ERROR_MSG: "Please use only English letters",
 
     JOB_NAME_MAX_LENGTH: 70,
