6e0bf44533875efeca2eb0eb2391d459e2328dda
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-29 13:09:45 +0200

    Component rebilling

--- src/components/EditJobListComponent/EditJobListComponent.js
+++ src/components/EditJobListComponent/EditJobListComponent.js
@@ -1,63 +1,71 @@
-import { useContext, useState } from 'react';
-import {JobListContext} from '../../context/JobListContext';
-import './EditJobListComponent.css';
+import { useContext, useState } from "react";
+import { JobListContext } from "../../context/JobListContext";
+import TableComponent from "../TableComponent/TableComponent";
 
-const EditJobListComponent = ({currentData}) => {
-    const [isOpen, setIsOpen] = useState(true);
-    const {data, editJob} = useContext(JobListContext);
-    const[inputs, setInputs] = useState({ priority:''});
+import "./EditJobListComponent.css";
 
-    const id = currentData.map(x=> x.id).pop();
-    const job = currentData.map(x =>x.job).pop();
-    const priority = inputs.priority;
+const EditJobListComponent = ({ currentData }) => {
 
-    const changeHandler = (e) => {
-        setInputs({
-            ...inputs,
-            [e.target.name]: e.target.value
-        })
-    };
-    const updatedJob = {id, job, priority };
+  const [isOpen, setIsOpen] = useState(true);
+  const { editJob } = useContext(JobListContext);
+  const [inputs, setInputs] = useState({ priority: "" });
+
+  const id = currentData.map((x) => x.id).pop();
+  const job = currentData.map((x) => x.job).pop();
+  const priority = inputs.priority;
+
+  const changeHandler = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const updatedJob = { id, job, priority };
+
+  const closeHandler = () => {
+    setIsOpen(false);
+    window.location.reload();
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    editJob(id, updatedJob);
+    setIsOpen(true);
     
+  };
 
-    const closeHandler = () => {
-        setIsOpen(false)
-        window.location.reload();
-    };
+  return isOpen ? (
+    <div className="priority_edit">
+      {currentData.map((currentJob) => (
+        <form onSubmit={onSubmit} key={currentJob.id}>
+          <div className="close_btn_container">
+            <button className="close_btn" type="button" onClick={closeHandler}>
+              x
+            </button>
+          </div>
 
-    const onSubmit = (e)=> {
-        e.preventDefault();
-        editJob(id, updatedJob)       
-    };
-  
- 
-    return isOpen ? (
-        <div className="priority_edit">
-            <form onSubmit={onSubmit}>
-            
-            <div className='close_btn_container'>
-                <button className='close_btn' type='submit' onClick={closeHandler}>x</button>
+          <div className="border">
+            <h4 className="priority_title">{currentJob.job}</h4>
+            <select
+              name="priority"
+              className="priority_select"
+              onChange={changeHandler}
+              defaultValue={currentJob.priority}
+            >
+              <option value={"Urgent"}>Urgent</option>
+              <option value={"Regular"}>Regular</option>
+              <option value={"Trivial"}>Trivial</option>
+            </select>
+            <div className="edit_btn_container">
+              <button className="update_btn" type="submit" title="Update item">
+                Update
+              </button>
             </div>
-                {currentData.map((currentJob)=>
-            <div className='border' key={currentJob.id}>
-                <h4 className="priority_title">{currentJob.job}</h4>
-                <select name="priority" className='priority_select'
-                onChange={changeHandler}
-                defaultValue={currentJob.priority}>
-                
-                    <option value={'Urgent'} >Urgent</option>
-                    <option value={"Regular"}>Regular</option>
-                    <option value={'Trivial'}>Trivial</option>
-                </select>
-                <div className='edit_btn_container' key={currentJob.id}>
-                    <button className='update_btn' type='submit' title='Update item'>Update</button>
-                </div>
-            </div>
-            )}
-      
+          </div>
         </form>
-        </div>
-    ) : null
+      ))}
+    </div>
+  ) : null
 };
 
 export default EditJobListComponent;
