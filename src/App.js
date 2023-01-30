fbb1641d4a704b82d62631472bd7a74beab54df0
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-30 02:46:25 +0200

    Add Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import JobListContextProvider from './context/JobListContext';
 import Home from './components/Home/Home';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   
   return (
     <div className="container">
       <JobListContextProvider>
-        <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
       </JobListContextProvider>
     </div>
   );
