2ddf5a9b439e6736b6cf6deb64c16c13d224644d
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-30 02:44:28 +0200

    Create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-item">
+                <img className="footer-icon" alt="footer-img" src="job.png"></img>
+                <p className="copyright">JOB LIST APPLICATION &copy; 2023 </p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
