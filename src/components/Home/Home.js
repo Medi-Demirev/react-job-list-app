e4f65dd848039989f8999c2f79c508340b785739
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-30 02:45:11 +0200

    Add main title for app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import AddJobComponent from '../AddJobComponent/AddJobComponent';
+import Footer from '../Footer/Footer';
 import './Home.css'
 
 const Home = () =>{
 
     return(
         <div className="container_wrap">
+            <h1 className='main_title'>JOB LIST </h1>
             <AddJobComponent/>
+            <Footer/>
         </div>
     )
 };
